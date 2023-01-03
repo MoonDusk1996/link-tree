9df0223902a71f172bb1a29b90e7f2d8afedf406
Dusk <91294230+MoonDusk1996@users.noreply.github.com>
2023-01-03 13:13:24 -0400

    fixes

--- services/UserContext.tsx
+++ services/UserContext.tsx
@@ -3,24 +3,16 @@ import React, { useState, createContext, useEffect } from "react";
 export const UserContext = createContext<any>({});
 
 function UserProvider({ children }: { children: React.ReactNode }) {
-  const [data, setData] = useState<number>();
+  const [data, setData] = useState<any>();
   const [isFething, setIsFething] = useState(false);
+  
+  
+  useEffect(() => {
+    fetch('/api/userdata')
+    .then((res)=>res.json())
+    .then((data)=>console.log(data))
 
-  function User() {
-    useEffect(() => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (pos) {
-          setData(pos.coords.latitude);
-        });
-      }
-
-      //   fetch("./api/userdata").then((res) =>
-      //     res.json().then((data) => {
-      //       console.log(data);
-      //     })
-      //   );
-    });
-  }
+  });
 
   return (
     <UserContext.Provider
