380a056358da46c60bc3b565327a871b2f998012
Dusk <91294230+MoonDusk1996@users.noreply.github.com>
2023-01-04 01:11:54 -0400

    refac: fetchAPI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AppThemeProvider } from "../contexts/themecontext";
-import UserProvider from "../services/UserContext";
+import useFetch from "../hooks/useFetch";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const data = useFetch()
   return (
     <AppThemeProvider>
-      <UserProvider>
         <Component {...pageProps} />
-      </UserProvider>
     </AppThemeProvider>
   );
 }
